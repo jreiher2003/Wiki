9203a8cc8aad800e26c06e2e89501b51894af745
Jeff Reiher <jreiher2003@yahoo.com>
2016-03-25 14:39:14 -0400

    Start on login - need to break

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
-from app import app 
-from flask import render_template, url_for
+from app import app, db
+from flask import render_template, url_for, request
 from utils import *
 
 
@@ -11,8 +11,14 @@ def index():
 def signup():
     return render_template("signup.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    error = None 
+    if request.method == "POST":
+        user = Users.query.filter_by(name=request.form["username"]).first()
+        if user is not None:
+            pass
+
     return render_template("login.html")
 
 @app.route("/logout")
