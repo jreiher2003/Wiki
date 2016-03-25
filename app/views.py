55d49ca51507befaefc4c40900ae150af6cda2df
Jeff Reiher <jreiher2003@yahoo.com>
2016-03-25 11:52:37 -0400

    Add regex to url and utils

--- app/views.py
+++ app/views.py
@@ -1,6 +1,25 @@
 from app import app 
+from flask import render_template, url_for
+from utils import *
 
 
 @app.route("/")
 def index():
-    return "This is jeff's Wiki" 
+    return render_template("index.html")
+
+@app.route("/signup")
+def signup():
+    return "this is signup page"
+
+@app.route("/login")
+def login():
+    return "this is login page"
+
+@app.route("/logout")
+def logout():
+    return "this is logout page"
+
+@app.route("/_edit/<regex(r'(?:[a-zA-Z0-9_-]+/?)'):param>")
+def edit_wiki(param):
+    return param
+
