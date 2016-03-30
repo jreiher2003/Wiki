2e9c7427c2744dd57dd9a21e2fb97885c3d9842e
Jeff Reiher <jreiher2003@yahoo.com>
2016-03-30 12:03:09 -0400

    Remove version from starter script

--- create_db.py
+++ create_db.py
@@ -10,15 +10,15 @@ db.session.add(User(name="admin2", email="admin2@admin.com", password="admin2"))
 db.session.add(User(name="admin3", email="admin3@admin.com", password='admin3'))
 print "just added a user"
 
-db.session.add(Wiki(wiki_post="this is a post", page_name="testpage", user_id=1, version=1))
-db.session.add(Wiki(wiki_post="another test post", page_name="testpage2", user_id=1, version=1))
+db.session.add(Wiki(wiki_post="this is a post", page_name="testpage", user_id=1))
+db.session.add(Wiki(wiki_post="another test post", page_name="testpage2", user_id=1))
 print "just added a wikipost"
 
-db.session.add(WikiRevisions(wiki_parent=1, wiki_post_rev="this is a revised post", user_id=1))
-db.session.add(WikiRevisions(wiki_parent=1, wiki_post_rev="this is another revised post", user_id=1))
-db.session.add(WikiRevisions(wiki_parent=1, wiki_post_rev="this is yet another revised post", user_id=1))
+db.session.add(WikiRevisions(wiki_parent=1, wiki_post_rev="this is a post", user_id=1))
+# db.session.add(WikiRevisions(wiki_parent=1, wiki_post_rev="this is another revised post", user_id=1))
+# db.session.add(WikiRevisions(wiki_parent=1, wiki_post_rev="this is yet another revised post", user_id=1))
 
-db.session.add(WikiRevisions(wiki_parent=2, wiki_post_rev="post 2 bla blas", user_id=2))
-db.session.add(WikiRevisions(wiki_parent=2, wiki_post_rev="post 2 bla blas yep yep revised", user_id=2))
+db.session.add(WikiRevisions(wiki_parent=2, wiki_post_rev="another test post", user_id=2))
+# db.session.add(WikiRevisions(wiki_parent=2, wiki_post_rev="post 2 bla blas yep yep revised", user_id=2))
 
 db.session.commit()
