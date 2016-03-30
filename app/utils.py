5be1a3071350fcaf706852b4b0d9c0f5063fc567
Jeff Reiher <jreiher2003@yahoo.com>
2016-03-30 12:02:46 -0400

    Remove regex url route code

--- app/utils.py
+++ app/utils.py
@@ -3,13 +3,13 @@ from app import app
 from werkzeug.routing import BaseConverter
 
 
-class RegexConverter(BaseConverter):
-    def __init__(self, url_map, *items):
-        super(RegexConverter, self).__init__(url_map)
-        self.regex = items[0]
+# class RegexConverter(BaseConverter):
+#     def __init__(self, url_map, *items):
+#         super(RegexConverter, self).__init__(url_map)
+#         self.regex = items[0]
 
-# Use the RegexConverter function as a converter
-# method for mapped urls
-app.url_map.converters['regex'] = RegexConverter
+# # Use the RegexConverter function as a converter
+# # method for mapped urls
+# app.url_map.converters['regex'] = RegexConverter
 
 
